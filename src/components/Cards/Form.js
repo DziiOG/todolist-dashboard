bfbd0f71e5c5e2db0ac997953961d5c35b2d3ad0
Pontiff Sulyvahn <workwithwhitson@gmail.com>
2022-05-01 19:48:48 +0000

    update

--- src/components/Cards/Form.js
+++ src/components/Cards/Form.js
@@ -2,17 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Box, Heading } from '@chakra-ui/react'
 
-const FormCard = ({ children, title, w, textAlign }) => (
+const FormCard = ({ children, title, w, textAlign, pt, mt }) => (
   <Box
     w={w}
     zIndex={0}
     bg='#FFFFFF 0% 0% no-repeat padding-box'
     p={{ base: 4, md: 8 }}
     mx={{ base: 'auto', md: 'initial' }}
-    rounded='md'
+    rounded='2xl'
     boxShadow='0px 1px 4px #F2F5FF33'
+    pt={pt}
   >
-    <Heading as='h6' textAlign={textAlign} fontSize={{ base: 'lg', md: 'xl' }}>
+    <Heading
+      mt={mt}
+      fontFamily='CaligatureHunters'
+      as='h6'
+      textAlign={textAlign}
+      color='#29325a'
+      fontSize={{ base: 'lg', md: '5xl' }}
+    >
       {title}
     </Heading>
     {children}
@@ -21,8 +29,10 @@ const FormCard = ({ children, title, w, textAlign }) => (
 
 FormCard.propTypes = {
   children: PropTypes.node.isRequired,
-  title: PropTypes.string.isRequired,
+  mt: PropTypes.any,
+  pt: PropTypes.any,
   textAlign: PropTypes.string,
+  title: PropTypes.string.isRequired,
   w: PropTypes.any
 }
 
--- src/pages/authenticate/index.js
+++ src/pages/authenticate/index.js
@@ -1,6 +1,9 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Icon } from '@chakra-ui/react'
+import Button from 'components/Button'
 import FormCard from 'components/Cards/Form'
+import { rem } from 'helpers/misc'
 import React from 'react'
+import { FcGoogle } from 'react-icons/fc'
 
 const Authenticate = () => (
   <Flex as='main' h='100vh' bg='gray.100' align='center' direction='column'>
@@ -13,7 +16,25 @@ const Authenticate = () => (
       px={{ base: 4, md: 20 }}
     />
     <Flex py={{ base: 4, lg: 10 }} w='full' align='center' justify='center'>
-      <FormCard title='Sign in' w={{ base: '90%', lg: 110 }} />
+      <FormCard
+        mt={12}
+        textAlign='center'
+        title='Todolist'
+        w={{ base: '90%', lg: 110 }}
+      >
+        <Button
+          bg='#FFFFFF 0% 0% no-repeat padding-box'
+          fontSize='md'
+          fontFamily='Avenir'
+          fontStyle='normal'
+          h={{ ...rem(48) }}
+          color='#29325A'
+          w={'100%'}
+          border='2px solid #677ACB'
+          title='Continue with google'
+          leftIcon={<Icon as={FcGoogle} boxSize={6} />}
+        />
+      </FormCard>
     </Flex>
   </Flex>
 )
