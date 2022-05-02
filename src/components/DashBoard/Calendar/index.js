74a261aaf94fb612eccc441878a0187a706a231b
Pontiff Sulyvahn <workwithwhitson@gmail.com>
2022-05-02 03:34:46 +0000

    update

--- src/components/DashBoard/Calendar/index.js
+++ src/components/DashBoard/Calendar/index.js
@@ -1,38 +1,56 @@
 import { Box } from '@chakra-ui/react'
 import Calendar from 'components/Calendar'
+import Spinner from 'components/FetchCard/Spinner'
+import useApi from 'context/useApi'
+import useAuth from 'context/useAuth'
 import useCalendar from 'hooks/useCalendar'
+import moment from 'moment'
 import React from 'react'
+import { useQuery } from 'react-query'
 // #67CBAC1A 0% 0% no-repeat padding-box;
 function CalenderDashboard() {
   const { slots, ...rest } = useCalendar()
+  const { getTasks } = useApi()
+  const { isAuthenticated } = useAuth()
+  const { user } = isAuthenticated()
+  const { data, isLoading, error, refetch } = useQuery(
+    [`tasks_user${user?._id}`],
+    async () => {
+      if (user?._id) {
+        return await getTasks({ user: user?._id })
+      }
+    },
+    {
+      staleTime: 90000
+    }
+  )
   return (
     <Box>
-      <Calendar
-        engine={{
-          slots: slots?.map(slot => ({
-            ...slot,
-            events: [
-              {
-                name: 'test',
-                progressValue: 0
-              },
-              {
-                name: 'test',
-                progressValue: 0
-              },
-              {
-                name: 'test',
-                progressValue: 0
-              },
-              {
-                name: 'test',
-                progressValue: 0
-              }
-            ]
-          })),
-          ...rest
+      <Spinner
+        w='100%'
+        h='md'
+        hook={{
+          loading: isLoading,
+          error,
+          triggerReload: () => refetch()
         }}
-      />
+      >
+        <Calendar
+          engine={{
+            slots: slots?.map(slot => ({
+              ...slot,
+              events: data?.data
+                ?.filter(task => {
+                  const taskDate = task?.dueDate
+                  const taskDateObj = moment(taskDate).format('MM-DD-YYYY')
+                  return slot?.date === taskDateObj
+                })
+                .map(item => ({ name: item.name, task: item }))
+            })),
+            ...rest
+          }}
+        />
+      </Spinner>
     </Box>
   )
 }
