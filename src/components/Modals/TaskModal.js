8ce4d37fdc2d20e32c1d6eed4ae2fcd5a198cf25
Pontiff Sulyvahn <workwithwhitson@gmail.com>
2022-05-02 09:55:59 +0000

    update

--- src/components/Modals/TaskModal.js
+++ src/components/Modals/TaskModal.js
@@ -71,6 +71,7 @@ const TaskModal = () => {
     ) => {
       try {
         setSubmitting(true)
+        console.log(values, 'itmsssss')
         const res = modalData
           ? await updateTask(modalData?._id, values)
           : await createTask(values)
